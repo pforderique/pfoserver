80b39ba40339c0ba6dade230480f48021e98bdf6
pforderique <porderiq@mit.edu>
2021-05-31 23:50:58 -0400

    nginx error fix attempt 2

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-from app import app
+from flask import Flask, jsonify, request, render_template
+from flask_restful import Api
+from apis.config import SECRET_KEY
+from apis.airchat import User
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
+
+api = Api(app)
+api.add_resource(User, "/user/", "/user/<id>")
+
+@app.route("/")
+def index():
+    return "<html><body><h1>Test site running under Flask :) - Piero!</h1></body></html>"
+    
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
