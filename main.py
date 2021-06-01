a600fe3accb46178d7ce7d6c5e3c5f4290d1d293
pforderique <porderiq@mit.edu>
2021-05-31 22:11:39 -0400

    using cloud

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 from flask import Flask, jsonify, request, render_template
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from pymongo import MongoClient
-
-#db = MonoClient()
+from config import SECRET_KEY, MONGO_URI, CLUSTER_NAME
+from apis.airchat import wassup
 
 app = Flask(__name__)
-#app.config['MONGO_URI'] =
-cluster = MongoClient("mongodb://localhost:27017")
-#mongo = PyMongo(app)
+app.config['SECRET_KEY'] = SECRET_KEY
+cluster = MongoClient(MONGO_URI)
 
-#db = mongo.db
-#users = db.users
-db = cluster['pfoserver']
+db = cluster[CLUSTER_NAME]
 users = db['users']
-
 
 @app.route("/")
 def index():
-    return "<html><body><h1>Test site running under Flask - Piero</h1></body></html>"
+    return "<html><body><h1>Test site running under Flask - Piero!</h1></body></html>"
+
+@app.route("/see-users")
+def see_users():
+    output = [{'name':user.get('name'), 'age':user.get('age')} for user in users.find()]
+    return jsonify({'result': output})
 
 @app.route("/hello/<name>")
 def hello(name):
-    results = users.find({'name':name})
-    out = [{"name":user.get("name"), "age":user.get("age")} for user in results]
-    return jsonify({"result":out})
+    users.insert({'name':name, 'age':10})
+    # out = [{"name":user.get("name"), "age":user.get("age")} for user in results]
+    return jsonify({"response":"user added"})
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0',debug=True)
