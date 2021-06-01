9bfcadfe025880b74bb6b315ca084a43034bd6a8
pforderique <porderiq@mit.edu>
2021-06-01 12:49:19 -0400

    testing flask pymongo

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from flask import Flask, jsonify, request, render_template
 from flask_restful import Api
+from config import SECRET_KEY, MONGO_URI
 #from apis.config import SECRET_KEY
 #from apis import User
 import sys
 sys.path.append("/home/pi/pfoserver2/apis")
 
-from airchat import User
-from config import SECRET_KEY, MONGO_URI
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
 app.config['MONGO_URI'] = MONGO_URI
+
+from airchat import User
 
 api = Api(app)
 api.add_resource(User, "/user/", "/user/<id>")
